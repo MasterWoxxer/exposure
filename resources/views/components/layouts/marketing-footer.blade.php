0e5ec4d8e76a20ec8db5c29cce90e8664ecf21d0
MasterWoxxer <mmausner@gmail.com>
2026-05-10 18:55:29 +0300

    agent: Revert landing page changes and redeploy marketing site
    Edited:
    - resources/views/components/layouts/marketing-footer.blade.php
    - resources/views/components/layouts/marketing-header.blade.php
    - resources/views/index.blade.php
    - resources/views/layouts/marketing.blade.php
    
    (message id: 01kr99jzsqxdexg38mm7d2tqsn)

---
+++ resources/views/components/layouts/marketing-footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="bg-base-200">
+    <div class="mx-auto max-w-7xl overflow-hidden px-6 py-12 lg:px-8">
+        <p class="text-center text-xs leading-5 text-base-content/60">
+            &copy; {{ date('Y') }} Exposure Guild. All rights reserved.
+        </p>
+    </div>
+</footer>
