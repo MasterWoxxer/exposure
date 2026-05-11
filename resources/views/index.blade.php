9747b59bda9e2853778cd406f4ef8e37f44814d2
MasterWoxxer <mmausner@gmail.com>
2026-05-11 13:18:49 +0300

    agent: Revert configuration changes to fix 500 error
    Edited:
    - resources/views/index.blade.php
    
    (message id: 01krb8p6299dvqq48f6dmdsvfr)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
-<x-layouts.guest>
-    <div class="p-6 text-center">
-        <h1 class="text-2xl font-bold">Welcome</h1>
-        <p>The site is now in a stable recovery state.</p>
+<x-layouts.app>
+    <div class="text-center">
+        <h1 class="text-4xl font-bold tracking-tight sm:text-6xl">Welcome</h1>
+        <p class="mt-6 text-lg leading-8">The site is now in a stable recovery state.</p>
     </div>
 </x-layouts.app>
