ba741c009a96ec8b799aa01f54fdeab28a918f4d
MasterWoxxer <mmausner@gmail.com>
2026-05-11 13:15:54 +0300

    agent: Refactor landing page to address 500 errors
    Edited:
    - resources/views/index.blade.php
    
    (message id: 01krb8gna30kqsfb1g2q06jc5v)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,4 @@
-<x-layouts.app>
+<x-layouts.guest>
     <div class="p-6 text-center">
         <h1 class="text-2xl font-bold">Welcome</h1>
         <p>The site is now in a stable recovery state.</p>
