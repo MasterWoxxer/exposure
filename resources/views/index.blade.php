f3aa0983438c55f108717427b46650ff4d88884b
MasterWoxxer <mmausner@gmail.com>
2026-05-11 13:20:58 +0300

    agent: took longer loading, then... 500 server error.
    Edited:
    - routes/web.php
    - resources/views/index.blade.php
    
    (message id: 01krb8sn8kw7efvja6yqtc76s5)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,4 @@
-<x-layouts.app>
+<x-layouts.guest>
     <div class="text-center">
         <h1 class="text-4xl font-bold tracking-tight sm:text-6xl">Welcome</h1>
         <p class="mt-6 text-lg leading-8">The site is now in a stable recovery state.</p>
